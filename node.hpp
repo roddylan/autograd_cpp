bfd01a6488be477347337180d4b27b31808e71d3
Roderick Lan <rodericklan03@gmail.com>
2024-12-30 18:13:02 -0700

    started constructors

--- node.hpp
+++ node.hpp
@@ -18,13 +18,47 @@ enum class Operation {
 template <typename T>
 class Value {
 public:
+    /**
+     * @brief Empty construct a new Value object
+     * 
+     */
+    Value() : val{} {}
+
+    /**
+     * @brief Value construct a new Value object
+     * 
+     * @param val 
+     */
+    Value(const T &val) : val{val} {}
+
+    /**
+     * @brief Value construct a new Value object
+     * 
+     * @param val 
+     */
+    Value(Value<T> &rhs) : val{rhs.val} {}
+
+    /**
+     * @brief assignment
+     * 
+     * @param rval 
+     * @return Value<T>& 
+     */
+    Value<T> &operator=(const Value<T> &rhs) {
+        if (this == &rhs) {
+            return *this;
+        }
+        
+        this->val = rhs.val;
+        return *this;
+    }
+
 
 
 
 private:
     T val;
-
-
+    Operation op;
 };
 
 
