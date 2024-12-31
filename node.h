8000d15d7a4156789dc46196e92668ed6358a81c
Roderick Lan <rodericklan03@gmail.com>
2024-12-30 17:56:24 -0700

    started node header

---
+++ node.h
@@ -0,0 +1,28 @@
+#ifndef NODE_H_
+#define NODE_H_
+
+
+/**
+ * @brief 
+ * 
+ * @tparam T 
+ */
+template <typename T>
+class Value {
+public:
+
+
+
+private:
+    T val;
+
+
+};
+
+
+
+
+
+
+
+#endif
